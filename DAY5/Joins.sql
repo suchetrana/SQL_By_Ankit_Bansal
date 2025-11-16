545f47ac413046d03385938b2d7898f9aef4ab85
suchetrana <suchetrana27@gmail.com>
2025-11-16 22:41:12 +0530

    added some more joins

--- DAY5/Joins.sql
+++ DAY5/Joins.sql
@@ -35,3 +35,21 @@ CREATE TABLE returns
 select  od.order_date as order_date, re.return_date as return_date
 from orders as od inner join returns as re on od.order_id = re.order_id;
 
+SELECT od.order_id, od.customer_id, od.product_id, od.order_date,
+       re.return_date, re.refund_amount, re.return_reason
+FROM orders AS od
+         INNER JOIN returns AS re ON od.order_id = re.order_id;
+
+SELECT od.order_id, od.customer_id, od.product_id, od.order_date
+FROM orders AS od
+         LEFT JOIN returns AS re ON od.order_id = re.order_id
+WHERE re.order_id IS NULL;
+
+SELECT re.return_id, re.order_id, re.customer_id, re.return_date
+FROM orders AS od
+         RIGHT JOIN returns AS re ON od.order_id = re.order_id
+WHERE od.order_id IS NULL;
+
+SELECT customer_id, SUM(refund_amount) AS total_refund
+FROM returns
+GROUP BY customer_id;
