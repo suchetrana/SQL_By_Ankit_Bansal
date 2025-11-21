06d082a578a0fde70ef40aae50ca856e71a102d0
suchetrana <suchetrana27@gmail.com>
2025-11-21 21:45:57 +0530

    Practicing joins

--- DAY6/query.sql
+++ DAY6/query.sql
@@ -1,5 +1,19 @@
+create database day6;
+use day6;
 select sub
 from orders o
 left join  returns r on o.order = r.order
 group by o.category
 having count(distinct  r.return ) = 3;
+
+SELECT
+    o.customer_id
+FROM orders AS o
+         JOIN order_items AS oi
+              ON o.order_id = oi.order_id
+         JOIN products AS p
+              ON oi.product_id = p.product_id
+WHERE o.order_date >= '2024-01-01'
+  AND o.order_date < '2025-01-01'
+GROUP BY o.customer_id
+HAVING COUNT(DISTINCT p.product_id) = 3;
